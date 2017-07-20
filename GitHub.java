1caa5757062822c0e6de43037a080b3c1d3c271d
Justin Ocampo <justin@Justins-MacBook-Pro.local>
2017-07-20 14:50:13 -0400

    added array list

--- GitHub.java
+++ GitHub.java
@@ -1,5 +1,33 @@
 package hotlanta;
 
+import java.util.ArrayList;
+
 public class GitHub {
 
+	ArrayList<String> files;
+	
+	public GitHub() {
+		files = new ArrayList<>();
+	}
+	
+	public void add(String s) {
+		files.add(s);
+	}
+	
+	public void remove(String s) {
+		files.remove(s);
+	}
+	
+	public String toString() {
+	
+		String toReturn = "";
+		
+		for (String s : files) {
+			toReturn += s + "\n";
+		}
+		
+		return toReturn;
+	}
+	
+	
 }
