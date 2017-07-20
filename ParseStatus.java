f3dc2b9c45c5f31580484e5249f4713ab66a4c6e
Anna <anna.bergstrom@ibm.com>
2017-07-20 16:34:26 -0400

    parse status now takes in file name arguments

--- ParseStatus.java
+++ ParseStatus.java
@@ -1,47 +1,59 @@
-public static void main(String[] args) {
-	GitHub gitHub = new GitHub();
-	Path status = Paths.get("gitStatus.txt"); 
-	try (InputStream in Files.newInputStream(status);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-		String line = null;
-		Boolean untracked = false;
-		while ((line = reader.readLine()) != null) {
-			String fileName = null;
-			ModificationType modificationType = null;
-			ModifiedFile modifiedFile;
-			if (line.contains("modified")) {
-				fileName = line.split(":")[1];
-				modificationType = ModificationType.CHANGED;
-			} else if (line.contains("deleted")) {
-				fileName = line.split(":")[1];
-				modificationType = ModificationType.DELETED;
-			} else if (line.contains("Untracked files")) {
-				untracked = true;
-			} else if (untracked) {
-				fileName = line;
-				modificationType = ModificationType.ADDED;
-			}
-			if (fileName != null) {
-				if (gitHub.contains(fileName)) {
-					modifiedFile = gitHub.get(fileName);
-				} else {
-					modifiedFile = new ModifiedFile(fileName);
-				}
-				Path credentials = Paths.get("gitCredentials.txt");
-				String name;
-				String userName;
-				try (InputStream in Files.newInputStream(credentials);
-					BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-					name = reader.readLine();
-					userName = reader.readLine();
-				} catch (IOException e) {
-					System.err.println(x);
-				}
-				modifiedFile.updateEdits(name, userName, System.currentTimeMillis());
-			}
-		}
-	} catch (IOException x) {
-		System.err.println(x);
+public class ParseStatus() {
+
+	private Path status;
+	private Path credentials;
+	private List<ModifiedFile> modifiedFiles;
+
+	public ParseStatus(String statusFile, String credentialsFile) {
+		status = Paths.get(statusFile);
+		credentials = Paths.get(credentialsFile);
+		modifiedFiles = new ArrayList<>();
 	}
+
+	private void readFile() {
+		try (InputStream in Files.newInputStream(status);
+		BufferedReader statusReader = new BufferedReader(new InputStreamReader(in))) {
+			String line = null;
+			Boolean untracked = false;
+			while ((line = statusReader.readLine()) != null) {
+				String fileName = null;
+				ModificationType modificationType = null;
+				ModifiedFile modifiedFile;
+				if (line.contains("modified")) {
+					fileName = line.split(":")[1];
+					modificationType = ModificationType.CHANGED;
+				} else if (line.contains("deleted")) {
+					fileName = line.split(":")[1];
+					modificationType = ModificationType.DELETED;
+				} else if (line.contains("Untracked files")) {
+					untracked = true;
+				} else if (untracked) {
+					fileName = line;
+					modificationType = ModificationType.ADDED;
+				}
+				if (fileName != null) {
+					modifiedFile = new ModifiedFile(fileName);
+					String name;
+					String userName;
+					try (InputStream in Files.newInputStream(credentials);
+						BufferedReader credentialReader = new BufferedReader(new InputStreamReader(in))) {
+						name = credentialReader.readLine();
+						userName = credentialReader.readLine();
+					} catch (IOException e) {
+						System.err.println(x);
+					}
+					modifiedFile.updateEdits(name, userName, System.currentTimeMillis());
+					modifiedFiles.add(modifiedFile);
+				}
+			}
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+	}
+
+	public ArrayList<ModifiedFile> getModifiedFiles() {
+		return modifiedFiles;
+	}
+	
 
 }
