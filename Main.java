e0b4f701320534ecf3f3727624e8a69801f5da79
zach.halaby <zachary.halaby@ibm.com>
2017-07-20 14:41:30 -0400

    more basic stuff

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+    
+        System.out.println("starting analysis");
+
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
 
     }
 }
