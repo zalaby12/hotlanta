431fa5594f2e5691a84f18e91437b837fc7a3bfa
zach.halaby <zachary.halaby@ibm.com>
2017-07-20 15:26:03 -0400

    HELP MAIN

--- Main.java
+++ Main.java
@@ -1,13 +1,31 @@
 package hotlanta;
 
 public class Main {
+
+    private static GitHub githubSimulator;
+
+    private static String CURRENT_DIRECTORY;
+
     public static void main(String[] args) {
     
         System.out.println("starting analysis");
 
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        CURRENT_DIRECTORY = System.getProperty("user.dir");
+
+        githubSimulator = new GitHub();
     
-        
+        populateGithubSimulator();
 
     }
+
+    private static void populateGithubSimulator(String arg) {
+//        githubSimulator.add(new ModifiedFile
+    }
+
+    private static void populateGithubSimulator() {
+        populateGithubSimulator(null);
+    }
+
 }
