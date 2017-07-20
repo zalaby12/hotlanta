8c9295e6d46b692b056db2aed49dfdc26156a89e
Camille Harris <camilleharris@Camilles-MacBook-Pro.local>
2017-07-20 14:55:15 -0400

    file ModifiedFile class and helper class Editor added

---
+++ Editor.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Editor {
+    private String name;
+    private String email;
+    private Date timestamp;
+
+    public Editor(String n, String e, Date t) {
+        this.name = n;
+        this.email = e;
+        this.timestamp = t;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public Date getTimestamp() {
+        return this.timestamp;
+    }
+
+}
---
+++ ModifiedFile.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.*;
+
+public class ModifiedFile extends File {
+    private LinkedList<Editor> edits;
+
+    //constructor
+    public ModifiedFile(String pathname) {
+        super(pathname);
+        edits = new LinkedList<Editor>();
+    }
+
+    public LinkedList<Editor> getEdits() {
+       return edits;
+    }
+
+    public void updtateEdits(String name, String email, Date time ) {
+        Editor e = new Editor(name, email, time);
+        this.edits.addFirst(e);
+    }
+
+    public Editor getMostRecentEditor(){
+        return edits.getFirst();
+    }
+    public Date lastTimeModified(){
+        Editor e = getMostRecentEditor();
+        return e.getTimestamp();
+
+    }
+
+    public String lastEditorName() {
+        Editor e = getMostRecentEditor();
+        return e.getName();
+    }
+
+    public String lastEditorEmail() {
+        Editor e = getMostRecentEditor();
+        return e.getEmail();
+    }
+
+}
